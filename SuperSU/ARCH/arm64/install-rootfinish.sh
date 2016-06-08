c8e1ff7c9d5b957c41ce511c1e0a5d2ceeba6e52
dl200010 <dl200010@gmail.com>
2016-06-07 21:22:53 -0500

    Remove install-recovery.sh

--- SuperSU/ARCH/arm64/install-rootfinish.sh
+++ SuperSU/ARCH/arm64/install-rootfinish.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 
-
+rm /system/bin/install-recovery.sh
 rm /system/bin/app_process
 ln -s /system/xbin/daemonsu /system/bin/app_process
 mv /system/bin/app_process64 /system/bin/app_process64_original
